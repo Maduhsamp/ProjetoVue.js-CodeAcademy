dbd764cc71d60637481ddbd156940e5fd3b2a7a5
MaduhSamp <madusampietro@gmail.com>
2024-06-10 17:29:02 -0300

    Envio de atualizações

--- docker-vuejs/vue-app/src/App.vue
+++ docker-vuejs/vue-app/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Header />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from '@/components/Header.vue';
+export default {
+  components: {
+    Header,
   }
 }
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 </style>
---
+++ docker-vuejs/vue-app/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav>
+    <img src="../assets/LogoAnimePlaza.png" alt="Logo">
+      <h4>Início</h4>
+      <h4>Sobre</h4>
+      <select>
+        <option disabled selected>Gênero</option>
+        <option value="opcao2">Opção 2</option>
+        <option value="opcao3">Opção 3</option>
+      </select>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style lang="scss">
+  nav img {
+    margin-left: -10%;
+    width: 20%;
+    height: auto;
+  }
+
+  nav {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-style: normal;
+    background: #3D0E5B;
+    color: #fff;
+    width: 100%;
+    height: 20%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  nav select {
+    background: none;
+    border: none;
+    color: #fff;
+    width: 15%;
+    height: auto;
+  }
+</style>

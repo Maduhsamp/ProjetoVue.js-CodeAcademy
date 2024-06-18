635eac5ee3258a12572b5a76fe2e3ccb15960ef2
Gustavo Silva <gustavosilvam85@gmail.com>
2024-06-18 10:59:13 -0300

    Projeto Concluido

--- docker-vuejs/vue-app/src/App.vue
+++ docker-vuejs/vue-app/src/App.vue
@@ -23,5 +23,24 @@ export default {
   background-color: #18171F;
   font: poppins;
 }
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #2A232F #18171F;
+}
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  background: #18171F;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #2A232F;
+  border-radius: 20px;
+  border: 3px solid #18171F;
+}
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 </style>
